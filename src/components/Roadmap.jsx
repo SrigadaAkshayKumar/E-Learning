7325991d5c2746c93712948a2e38d44edf08231e
Srigada Akshay Kumar <153109964+SrigadaAkshayKumar@users.noreply.github.com>
2025-02-16 01:03:57 +0530

    Update Roadmap.jsx

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -13,8 +13,7 @@ const Roadmap = () => {
   const responseContainerRef = useRef(null);
 
   // Initialize Generative AI API
-  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API); // Replace with your actual API key
-  console.log("Gemini API Key:", process.env.NEXT_PUBLIC_GEMINI_API);
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API); 
   const model = genAI.getGenerativeModel({ model: "gemini-pro" }); // Use a supported model
 
   const handleSubmit = async (e) => {
